3133b09d1a573471a4a60ad4b435b951c9839cfc
recode-sm <recode-sm@naver.com>
2025-03-29 10:31:34 +0900

    UPDATE
    처음 환경 설정

---
+++ src/client/java/org/TheGoon/minegram/client/MinegramClient.java
@@ -0,0 +1,10 @@
+package org.TheGoon.minegram.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinegramClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/TheGoon/minegram/Minegram.java
@@ -0,0 +1,11 @@
+package org.TheGoon.minegram;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Minegram implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
